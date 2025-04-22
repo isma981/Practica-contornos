576b43dc7eb98146637de2fd13e3f61fd591faaf
isma <ismael.couso.10@gmail.com>
2025-04-22 19:34:42 +0200

    Calculadora con nuevas funciones

--- CalculadoraJava/src/utils/Operaciones.java
+++ CalculadoraJava/src/utils/Operaciones.java
@@ -8,4 +8,7 @@ public class Operaciones {
     public static int restar(int a, int b) {
         return a - b;
     }
+  public static double multiplicar(double a, double b) {
+        return a * b;
+}
 }
